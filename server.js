24e9df3fab6678d442997594d61ea46990191770
Tina Davis <christina.davis@libertymutual.com>
2024-04-23 15:39:29 -0400

    completed stage01 basic file server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = 4000;
 
 var app = express();
 
-app.use();
+app.use(express.static('public'));             // URL '/' (root) maps to 'public' directory
+app.use('/static', express.static('public'));  // URL '/static' also maps to 'public' directory
+
 
 app.listen(port);
